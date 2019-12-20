17511dafa19dbf5f964c2264a01682854bf6a669
neil <s1k2y37st@gmail.com>
2019-12-20 18:58:26 +0800

    remove unused code

--- TwitchRoyalty/background.js
+++ TwitchRoyalty/background.js
@@ -5,28 +5,15 @@ var alarmInfo = {
 
 chrome.alarms.clearAll();
 chrome.alarms.onAlarm.addListener(function(alarm) {
-    //計算定時器觸發次數
-    console.log("onAlarm-");
     chrome.tabs.query({}, function(tabs) { 
         tabs.forEach(tab => {
             var url = new URL(tab.url);
             var domain =  url.hostname;
             if(domain === "www.twitch.tv"){
-                console.log(tab)
+                console.log("Get Twitch Pages:" + url);
                 chrome.tabs.executeScript(tab.id, {file: "execute.js"});
             }
         });
     } );
-    // chrome.tabs.query({active: true, currentWindow: false}, function(tabs) {
-    //     // Injects JavaScript code into a page
-    //     console.log(tabs);
-
-    //     tabs.forEach(tab => {
-    //         var url = new URL(tab.url);
-    //         var domain =  url.hostname;
-    //         console.log(domain);
-    //     });
-    //     // 
-    // });
 });
 chrome.alarms.create('testAlarm',alarmInfo);
--- TwitchRoyalty/execute.js
+++ TwitchRoyalty/execute.js
@@ -1,9 +1,8 @@
 function goToActivityTab() {
     try {
         var activityTab = document.getElementsByClassName("tw-button tw-button--success tw-interactive")[0];
-
         activityTab.click();
-        console.log("auto click special button!!")
+        console.log("Auto click gift button!!")
     } catch (error) {
         
     }
